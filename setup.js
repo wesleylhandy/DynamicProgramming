80becaed097a36987d309e8a94e31e5042c86f48
Wesley Handy <whandy@costar.com>
2023-10-06 10:17:25 -0400

    Match file names to function names

--- setup.js
+++ setup.js
@@ -1,30 +1,30 @@
-const BruteForceWordsFromWordBank = require('./problems/word-construction')
-const MemoizedWordsFromWordBank = require('./solutions/memoized/word-construction')
-const TabulatedWordsFromWordBank = require('./solutions/tabulated/word-construction')
+const BruteForceWordsFromWordBank = require('./problems/words-from-word-bank')
+const MemoizedWordsFromWordBank = require('./solutions/memoized/words-from-word-bank')
+const TabulatedWordsFromWordBank = require('./solutions/tabulated/words-from-word-bank')
 
-const BruteForceBestAddends = require('./problems/best-sum');
-const MemoizedBestAddends = require('./solutions/memoized/best-sum');
-const TabulatedBestAddends = require('./solutions/tabulated/best-sum');
+const BruteForceBestAddends = require('./problems/best-addends-for-sum');
+const MemoizedBestAddends = require('./solutions/memoized/best-addends-for-sum');
+const TabulatedBestAddends = require('./solutions/tabulated/best-addends-for-sum');
 
 const BruteForceCanConstruct = require("./problems/can-construct-word");
 const MemoizedCanConstruct = require("./solutions/memoized/can-construct-word");
 const TabulatedCanConstruct = require("./solutions/tabulated/can-construct-word");
 
-const BruteForceCanSum = require('./problems/can-sum');
-const MemoizedCanSum = require('./solutions/memoized/can-sum');
-const TabulatedCanSum = require('./solutions/tabulated/can-sum');
+const BruteForceCanSum = require('./problems/can-sum-target');
+const MemoizedCanSum = require('./solutions/memoized/can-sum-target');
+const TabulatedCanSum = require('./solutions/tabulated/can-sum-target');
 
-const BruteForceWordsCount = require('./problems/count-construct');
-const MemoizedWordsCount = require('./solutions/memoized/count-construct');
-const TabulatedWordsCount = require('./solutions/tabulated/count-construct');
+const BruteForceWordsCount = require('./problems/words-from-word-bank-count');
+const MemoizedWordsCount = require('./solutions/memoized/words-from-word-bank-count');
+const TabulatedWordsCount = require('./solutions/tabulated/words-from-word-bank-count');
 
 const BruteForceGridTraveler  = require('./problems/grid-traveler');
 const MemoizedGridTraveler = require('./solutions/memoized/grid-traveler');
 const TabulatedGridTraveler = require('./solutions/tabulated/grid-traveler');
 
-const BruteForceMakeSum = require('./problems/how-sum');
-const MemoizedMakeSum = require('./solutions/memoized/how-sum');
-const TabulatedMakeSum = require('./solutions/tabulated/how-sum');
+const BruteForceMakeSum = require('./problems/numbers-that-make-sum');
+const MemoizedMakeSum = require('./solutions/memoized/numbers-that-make-sum');
+const TabulatedMakeSum = require('./solutions/tabulated/numbers-that-make-sum');
 
 const BruteForceNthFibonacci = require('./problems/nth-fibonacci');
 const MemoizedNthFibonacci = require('./solutions/memoized/nth-fibonacci');
